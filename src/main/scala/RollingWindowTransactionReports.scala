561c035df1ca015ce66c6496925c7e75bb0255af
Zak Bibi <zak.bibi49@gmail.com>
2021-03-12 16:40:09 +0000

    Added a filler for the total transactions per account per caregory, and tests. Edited totalTransactionValuePerCategoryPerAccount to use the filler.

--- src/main/scala/RollingWindowTransactionReports.scala
+++ src/main/scala/RollingWindowTransactionReports.scala
@@ -10,7 +10,7 @@ class RollingWindowTransactionReports {
   }
 
   def maxTransactionValuePerAccount(transactions: List[Transaction]): Map[(Int, String), Double] = {
-    val filler = maxTransactionFiller(transactions)
+    val filler = maxAndAverageTransactionFiller(transactions)
       .transform((_, value) => value.map(e => e.transactionAmount).head)
 
     val maxTransactionValue = transactions.groupBy(t => (t.transactionDay, t.accountId))
@@ -24,12 +24,25 @@ class RollingWindowTransactionReports {
 
   }
 
-  def maxTransactionFiller(transactions: List[Transaction]): ListMap[(Int, String), List[Transaction]] = {
-    val byTransactionDay = transactions.groupBy(trans => trans.transactionDay)
-    val byAccountId = transactions.groupBy(trans => trans.accountId)
+  def averageTransactionValuePerAccount(transactions: List[Transaction]): Map[(Int, String), Double] = {
+    val filler = maxAndAverageTransactionFiller(transactions)
+      .transform((_, value) => value.map(e => e.transactionAmount).head)
 
-    val transactionDays = byTransactionDay.keySet
-    val accountIds = byAccountId.keySet
+    val transactionAverage = transactions.groupBy(t => (t.transactionDay, t.accountId))
+      .transform (
+      (_,value) => value.map(e => e.transactionAmount).sum / value.length
+    )
+
+    val filledTransactionValues = filler ++ transactionAverage
+
+    ListMap(filledTransactionValues.toSeq.sortBy(r => (r._1._1, r._1._2)):_*)
+
+  }
+
+  def maxAndAverageTransactionFiller(transactions: List[Transaction]): ListMap[(Int, String), List[Transaction]] = {
+
+    val accountIds = transactions.map(t => t.accountId).toSet
+    val transactionDays = transactions.map(t => t.transactionDay).toSet
 
     val daysAndIdKeys = for{
       accId <- accountIds
@@ -48,17 +61,41 @@ class RollingWindowTransactionReports {
 
   }
 
-  def averageTransactionValuePerAccount(transactions: List[Transaction]): Map[(Int, String), Double] = {
-    transactions.groupBy(t => (t.transactionDay, t.accountId))
-      .transform (
-      (_,value) => value.map(e => e.transactionAmount).sum / value.length
-    )
-  }
-
   def totalTransactionValuePerCategoryPerAccount(transactions: List[Transaction]): Map[(Int, String, String), Double] = {
-    transactions
+    val filler = totalTransValPerCategoryPerAccFiller(transactions)
+      .transform((_, value) => value.map(e => e.transactionAmount).head)
+
+    val totals  = transactions
       .groupBy(t => (t.transactionDay, t.accountId, t.category))
       .transform((_, value) => value.map(e => e.transactionAmount).sum)
+
+    val filledTotals = filler ++ totals
+
+    ListMap(filledTotals.toSeq.sortBy(r => (r._1._1, r._1._2, r._1._3)):_*)
+  }
+
+  def totalTransValPerCategoryPerAccFiller(transactions: List[Transaction]): ListMap[(Int, String, String), List[Transaction]] = {
+
+    val accountIds = transactions.map(t => t.accountId).toSet
+    val transactionDays = transactions.map(t => t.transactionDay).toSet
+    val categories = transactions.map(t => t.category).toSet
+
+    val daysAndIdKeys = for{
+      accId <- accountIds
+      transDay <- transactionDays
+      category <- categories
+    } yield (transDay, accId, category)
+
+    val fillTransactions = daysAndIdKeys
+      .toList
+      .map(t => (t, List(Transaction("", "", 0, "", 0))))
+
+    val filledMap = fillTransactions
+      .groupBy(_._1)
+      .transform((_,v) => v.flatMap(e => e._2))
+
+    ListMap(filledMap.toSeq.sortBy(r => (r._1._1, r._1._2, r._1._3)):_*)
+
   }
 
 }
--- src/test/scala/RollingWindowTransactionReportsSpec.scala
+++ src/test/scala/RollingWindowTransactionReportsSpec.scala
@@ -37,7 +37,7 @@ class RollingWindowTransactionReportsSpec extends AnyFlatSpec with Matchers {
     )
   }
 
-  "dataFillers" should "fill in the empty data for max transactions" in {
+  "dataFillers" should "fill in the empty data for max and average transactions" in {
     val data = List (
       Transaction("T0001", "A1", 1, "AA", 10.00),
       Transaction("T0001", "A1", 2, "BB", 10.00),
@@ -45,13 +45,38 @@ class RollingWindowTransactionReportsSpec extends AnyFlatSpec with Matchers {
       Transaction("T0001", "A3", 2, "BB", 10.00)
     )
 
-    rw.maxTransactionFiller(data) shouldBe ListMap(
+    rw.maxAndAverageTransactionFiller(data) shouldBe ListMap(
       (1, "A1") -> List(Transaction("", "", 0, "", 0.0)),
       (1, "A2") -> List(Transaction("", "", 0, "", 0.0)),
       (1, "A3") -> List(Transaction("", "", 0, "", 0.0)),
       (2, "A1") -> List(Transaction("", "", 0, "", 0.0)),
       (2, "A2") -> List(Transaction("", "", 0, "", 0.0)),
       (2, "A3") -> List(Transaction("", "", 0, "", 0.0))
+    )
+
+  }
+
+  it should "fill in the empty data for total transactions" in {
+    val data = List (
+      Transaction("T0001", "A1", 1, "AA", 10.00),
+      Transaction("T0001", "A1", 2, "BB", 10.00),
+      Transaction("T0001", "A2", 1, "AA", 10.00),
+      Transaction("T0001", "A3", 2, "BB", 10.00)
+    )
+
+    rw.totalTransValPerCategoryPerAccFiller(data) shouldBe ListMap(
+      (1, "A1", "AA") -> List(Transaction("", "", 0, "",0.0)),
+      (1, "A1", "BB") -> List(Transaction("", "", 0, "",0.0)),
+      (1, "A2", "AA") -> List(Transaction("", "", 0, "",0.0)),
+      (1, "A2", "BB") -> List(Transaction("", "", 0, "",0.0)),
+      (1, "A3", "AA") -> List(Transaction("", "", 0, "",0.0)),
+      (1, "A3", "BB") -> List(Transaction("", "", 0, "",0.0)),
+      (2, "A1", "AA") -> List(Transaction("", "", 0, "",0.0)),
+      (2, "A1", "BB") -> List(Transaction("", "", 0, "",0.0)),
+      (2, "A2", "AA") -> List(Transaction("", "", 0, "",0.0)),
+      (2, "A2", "BB") -> List(Transaction("", "", 0, "",0.0)),
+      (2, "A3", "AA") -> List(Transaction("", "", 0, "",0.0)),
+      (2, "A3", "BB") -> List(Transaction("", "", 0, "",0.0))
     )
 
   }
@@ -84,7 +109,7 @@ class RollingWindowTransactionReportsSpec extends AnyFlatSpec with Matchers {
   }
 
   "averageTransactionValuePerAccount" should "find the averages of each account" in {
-    val data = List (
+    val data = List(
       Transaction("T0001", "A1", 1, "AA", 30.00),
       Transaction("T0001", "A1", 2, "BB", 50.00),
       Transaction("T0001", "A2", 3, "CC", 90.00),
@@ -95,28 +120,49 @@ class RollingWindowTransactionReportsSpec extends AnyFlatSpec with Matchers {
       Transaction("T0001", "A1", 8, "BB", 90.00)
     )
     val windowedData = rw.rollingWindow(data, 8, 5)
-     rw.averageTransactionValuePerAccount(windowedData) shouldBe Map(
-       (7, "A2") -> 30.0,
-       (6, "A1") -> 40.0,
-       (5, "A2") -> 100.0,
-       (4, "A1") -> 10.0,
-       (3, "A2") -> 90.0)
-  }
-
-  "totalTransactionValuePerCategoryPerAccount" should "find the total per transaction value" in {
-    val data = List(
-      Transaction("T0001", "A2", 7, "GG", 10.0),
-      Transaction("T0001", "A1", 6, "FF", 10.0),
-      Transaction("T0001", "A2", 3, "AA", 10.0),
-      Transaction("T0001", "A1", 4, "DD", 10.0),
-      Transaction("T0001", "A2", 3, "AA", 10.0)
+    rw.averageTransactionValuePerAccount(windowedData) shouldBe ListMap(
+      (3, "A1") -> 0.0,
+      (3, "A2") -> 90.0,
+      (4, "A1") -> 10.0,
+      (4, "A2") -> 0.0,
+      (5, "A1") -> 0.0,
+      (5, "A2") -> 100.0,
+      (6, "A1") -> 40.0,
+      (6, "A2") -> 0.0,
+      (7, "A1") -> 0.0,
+      (7, "A2") -> 30.0
     )
-
-    rw.totalTransactionValuePerCategoryPerAccount(data) shouldBe Map (
-      (4, "A1", "DD") -> 10.0,
-      (6, "A1", "FF") -> 10.0,
-      (7, "A2", "GG") -> 10.0,
-      (3, "A2", "AA") -> 20.0)
   }
+
+    "totalTransactionValuePerCategoryPerAccount" should "find the total per transaction value" in {
+      val data = List(
+        Transaction("T0001", "A1", 1, "AA", 10.00),
+        Transaction("T0001", "A1", 1, "AA", 10.00),
+        Transaction("T0001", "A1", 1, "BB", 5.00),
+        Transaction("T0001", "A1", 1, "BB", 5.00),
+        Transaction("T0001", "A2", 2, "BB", 10.00),
+        Transaction("T0001", "A3", 3, "AA", 10.00)
+      )
+
+      rw.totalTransactionValuePerCategoryPerAccount(data) shouldBe ListMap(
+        (1, "A1", "AA") -> 20.0,
+        (1, "A1", "BB") -> 10.0,
+        (1, "A2", "AA") -> 0.0,
+        (1, "A2", "BB") -> 0.0,
+        (1, "A3", "AA") -> 0.0,
+        (1, "A3", "BB") -> 0.0,
+        (2, "A1", "AA") -> 0.0,
+        (2, "A1", "BB") -> 0.0,
+        (2, "A2", "AA") -> 0.0,
+        (2, "A2", "BB") -> 10.0,
+        (2, "A3", "AA") -> 0.0,
+        (2, "A3", "BB") -> 0.0,
+        (3, "A1", "AA") -> 0.0,
+        (3, "A1", "BB") -> 0.0,
+        (3, "A2", "AA") -> 0.0,
+        (3, "A2", "BB") -> 0.0,
+        (3, "A3", "AA") -> 10.0,
+        (3, "A3", "BB") -> 0.0)
+    }
 
 }
